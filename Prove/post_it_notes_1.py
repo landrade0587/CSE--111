24af950c04fcca31d7517d2d2ca5d9c37fd79d20
landrade0587 <and23002@byui.edu>
2023-07-02 13:06:52 -0500

    added changes this code

--- Prove/post_it_notes_1.py
+++ Prove/post_it_notes_1.py
@@ -1,20 +1,20 @@
-#Import Necessary modules
+#In this place we Import Necessary modules.
 import sqlite3 as sql
 from datetime import date
 from tkinter import *
 from tkinter import messagebox
 
-# Create database connection and connect to table
+# Now we Create database connection and connect to table.
 try:
-       con = sql.connect('Add_your_note.db')
+       con = sql.connect('pin_your_note.db')
        cur = con.cursor()
-       cur.execute('''CREATE TABLE notes_table
+       cur.execute('''CREATE TABLE notes_table 
                         (date DATETIME, notes_title text, notes text)''')
 except:
        print("Connected to table of database")
 
 
-# Insert a row of data
+#Rigth now We Insert a row of data.
 def add_notes():
        #Get input values
        today = date.today()
@@ -23,11 +23,11 @@ def add_notes():
        notes = notes_entry.get("1.0", "end-1c")
        #Raise a prompt for missing values
        if (len(notes_title)<=0) & (len(notes)<=1):
-               messagebox.showerror(message = "ENTER REQUIRED DETAILS" )
+               messagebox.showerror(message = "PLEASE ENTER REQUIRED DETAILS" )
        else:
        #Insert into the table
                cur.execute("INSERT INTO notes_table VALUES ('%s','%s','%s')" %(today, notes_title, notes))
-               messagebox.showinfo(message="Note added")
+               messagebox.showinfo(message="The Note has been added.")
        #Commit to preserve the changes
                con.commit()
 
@@ -50,13 +50,13 @@ def view_notes():
        else:
                sql_statement = "SELECT * FROM notes_table where date ='%s' and notes_title ='%s'" %(date, notes_title)
               
-       #Execute the query
+       #Execute the queryconfer
        cur.execute(sql_statement)
        #Obtain all the contents of the query
        row = cur.fetchall()
        #Check if none was retrieved
        if len(row)<=0:
-               messagebox.showerror(message="No note found")
+               messagebox.showerror(message="No note found. \n\n Sorry.")
        else:
                #Print the notes
                for i in row:
@@ -76,13 +76,13 @@ def delete_notes():
        #Delete notes matching a particular date and title
                if (len(notes_title)<=0): 
                        #Raise error for no inputs
-                       messagebox.showerror(message = "ENTER REQUIRED DETAILS" )
+                       messagebox.showerror(message = "PLEASE ENTER REQUIRED DETAILS" )
                        return
                else:
                       sql_statement = "DELETE FROM notes_table where date ='%s' and notes_title ='%s'" %(date, notes_title)
        #Execute the query
        cur.execute(sql_statement)
-       messagebox.showinfo(message="Note(s) Deleted complety")
+       messagebox.showinfo(message="The Note(s) have been completely Deleted")
        con.commit()
 
 
@@ -93,10 +93,10 @@ def update_notes():
        notes = notes_entry.get("1.0", "end-1c")
        #Check if input is given by the user
        if (len(notes_title)<=0) & (len(notes)<=1):
-               messagebox.showerror(message = "ENTER REQUIRED DETAILS" )
+               messagebox.showerror(message = "PLEASE ENTER REQUIRED DETAILS" )
        #update the note
        else:
-               sql_statement = "UPDATE notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(notes, today, notes_title)
+               sql_statement = "UPDATE notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(notes, notes_title)
               
        cur.execute(sql_statement)
        messagebox.showinfo(message="Note Updated")
@@ -106,9 +106,9 @@ def update_notes():
 #Invoke call to class to view a window
 window = Tk()
 #Set dimensions of window and title
-window.geometry("600x300")
-window.title("Add Your Note")
-title_label = Label(window, text="Please Add Your Note \n ").pack()
+window.geometry("500x300")
+window.title("Pin Your Note")
+title_label = Label(window, text="Pin Your Note").pack()
 
 #Read inputs
 #Date input
@@ -127,10 +127,10 @@ notes_entry = Text(window, width=60,height=5)
 notes_entry.place(x=10,y=120)
  
 #Perform notes functions
-button1 = Button(window,text='Add Notes', bg = 'gray',fg='black',command=add_notes).place(x=10,y=20)
-button2 = Button(window,text='View Notes', bg = 'gray',fg='black',command=view_notes).place(x=110,y=20)
-button3 = Button(window,text='Delete Notes', bg = 'gray',fg='black',command=delete_notes).place(x=210,y=20)
-button4 = Button(window,text='Update Notes', bg = 'gray',fg='white',command=update_notes).place(x=320,y=20)
+button1 = Button(window,text='Add Notes', bg = 'Turquoise',fg='Black',command=add_notes).place(x=10,y=20)
+button2 = Button(window,text='View Notes', bg = 'Turquoise',fg='Blue',command=view_notes).place(x=110,y=20)
+button3 = Button(window,text='Delete Notes', bg = 'Turquoise',fg='Red',command=delete_notes).place(x=210,y=20)
+button4 = Button(window,text='Update Notes', bg = 'Turquoise',fg='Purple',command=update_notes).place(x=320,y=20)
  
 #close the app
 window.mainloop()
